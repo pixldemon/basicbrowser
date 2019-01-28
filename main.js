a40974d50717c7aaacd3445ce472c211e52d422e
pixldemon <34690317+pixldemon@users.noreply.github.com>
2019-01-28 08:37:24 +0100

    Added the browser top bar, initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const {app, BrowserWindow} = require("electron");
+
+let win;
+
+function createWindow() {
+
+	win = new BrowserWindow({width: 800, height: 600, frame: false, transparent: true});
+	win.loadFile("index.html");
+	
+}
+
+app.on('ready', function () {
+    setTimeout(function() {
+        createWindow();
+    }, 100);
+});
